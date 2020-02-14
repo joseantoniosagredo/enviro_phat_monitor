12bf955f26130226b3550a1add7da21051eb0ea7
Jose Antonio <jose.gutierrez.sagredo@gmail.com>
2020-02-14 21:46:47 +0100

    python3 implemented

--- src/bashConnector/pythonConnector.ts
+++ src/bashConnector/pythonConnector.ts
@@ -2,7 +2,7 @@ import { Callback } from "../utils/typescript";
 import { execFile } from 'child_process'
 
 export default function (script: string[], callback: Callback<string>) {
-    execFile('python', script,(err,data,errdata)=>{
+    execFile('python3', script,(err,data,errdata)=>{
         if(data) console.log(data)
         if(err) return console.log(err)
         if(errdata) return console.log(errdata)
