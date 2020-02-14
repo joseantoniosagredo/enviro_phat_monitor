4ae459e198c27dbfe35a7178fab861edc72160d1
Jose Antonio <jose.gutierrez.sagredo@gmail.com>
2020-02-14 21:41:02 +0100

    add env variables

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as  express from 'express'
 import { createInterval } from './bashConnector/envirophat';
 
+const PORT = process.env.PORT || 3000
+
 const app = express()
 createInterval()
-app.listen(80, () => console.log('Start application'))
+app.listen(PORT, () => console.log('Start application, PORT:' + PORT))
