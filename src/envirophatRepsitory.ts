701b6493b01ad2e22cf78b278623668ac65e7fb8
Jose Antonio <jose.gutierrez.sagredo@gmail.com>
2020-03-22 21:05:11 +0100

    Solver defect get last

--- src/envirophatRepsitory.ts
+++ src/envirophatRepsitory.ts
@@ -3,7 +3,7 @@ import { Callback } from "./utils/typescript";
 import { EnviroPhatType } from "./models/envirophat";
 
 export function getLastRegister(callback:Callback<EnviroPhatType |null>){
-    EnvirophatModel.find({},{},{$sort:{date:-1}}).limit(1).exec((err,data)=>{
+    EnvirophatModel.find().sort({date:-1}).limit(1).exec((err,data)=>{
         if(err) return callback(err)
         if(data.length===0) return callback(null,null)
         callback(null,data[0])
